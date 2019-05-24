9fd5407c151bd24a5d2b0286eacc9b48648cc124
m.kuznetsova@qpdev.ru <m.kuznetsova@qpdev.ru>
2019-05-24 19:03:20 +0300

    Add new assignments (project- TheSecondAssignment) there are five tests : UseFirefox, UseChrome,UseExplorer- the first part of assignment UseFirefoxESR - the second part (it doesn't work) UseFirefoxNightly - the fird part

---
+++ TheSecondAssignment/1.UseFirefox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace TheSecondAssignment
+{
+    [TestFixture(Description = "1. Авторизация в системе через Firefox "), Order(1)]
+    public class UseFirefox
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [Test(Description = "1. Открыть браузер"), Order(1)]
+        public void OpenBrowser()
+        {
+            driver = new FirefoxDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("http://localhost/litecart/admin/");
+            driver.Manage().Window.Maximize();
+        }
+        [Test(Description = "2. Авторизоваться в системе"), Order(2)]
+        public void AutorizationAsAnAdmin()
+        {
+            string login = "admin";
+            driver.FindElement(By.Name("username")).SendKeys(login);
+            driver.FindElement(By.Name("password")).SendKeys(login);
+            driver.FindElement(By.Name("login")).Click();
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+
+        }
+        [Test(Description = "3. Закрытие браузера"), Order(3)]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
---
+++ TheSecondAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TheSecondAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheSecondAssignment")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e39434a7-5a64-4ffe-b1f5-e30f9bd3854e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
