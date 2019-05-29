3b801aba4347dc4a3a499200b7945cb56b52e32a
m.kuznetsova@qpdev.ru <m.kuznetsova@qpdev.ru>
2019-05-29 17:17:45 +0300

    Fix the last assignment and add the new one

--- TheFirstAssignment/TheThirdClass/1.ClickAndCheck.cs
+++ TheFirstAssignment/TheThirdClass/1.ClickAndCheck.cs
@@ -10,16 +10,29 @@ using System.Threading;
 namespace TheFirstAssignment.TheThirdClass
 {
     [TestFixture(Description = "1. Авторизоваться, прокликать все пункты меню и проверить наличие заголовка "), Order(1)]
-    class ClickAndCheck : TestBase
+    class ClickAndCheck 
     {
-        
         private IWebDriver driver;
         private WebDriverWait wait;
+        By locator, locator1;
 
+        bool IsElementPresent(IWebDriver driver, By locator)
+        {
+            try
+            {
+                driver.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException ex)
+            {
+                return false;
+            }
+        }
 
         [Test(Description = "1. Открыть браузер"), Order(1)]
         public void OpenBrowser()
         {
+            
             driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             driver.Navigate().GoToUrl("http://localhost/litecart/admin/");
@@ -32,20 +45,35 @@ namespace TheFirstAssignment.TheThirdClass
             driver.FindElement(By.Name("login")).Click();
             wait.Until(ExpectedConditions.TitleIs("My Store"));
 
-
-            
-            for(int i=1; i <= GetCountOfMenuElements(driver);i++)
-            {   
-                GetMainItem(driver, i).Click();
-                for (int j=1; j<= GetCountOfElements(driver);j++)
+            locator = By.XPath("//li[contains(@id,'app-')]");
+            IList<IWebElement> listOfBarTitles = driver.FindElements(locator);
+            locator1 = By.XPath("//li[contains(@id,'doc-')]");
+            int countOfBarTitles = listOfBarTitles.Count;
+            if (countOfBarTitles > 0)
+            {
+                for (int i = 0; i < countOfBarTitles; i++)
                 {
-                   GetChildItem(driver, j).Click();
-                   Assert.True(IsElementPresent(driver, GetTitleLocator()), "Ожидалось, что заголовок будет видимым ");
+                    listOfBarTitles = driver.FindElements(locator);
+                    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                    listOfBarTitles[i].Click();
+                    IList<IWebElement> listOfNestedTitles = driver.FindElements(locator1);
+                    int countOfNestedTitles = listOfNestedTitles.Count;
+                    if (countOfNestedTitles > 0)
+                    {
+                        for (int j = 0; j < countOfNestedTitles; j++)
+                        {
+                            listOfNestedTitles = driver.FindElements(locator1);
+                            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                            listOfNestedTitles[j].Click();
+                            Assert.True(IsElementPresent(driver,By.XPath("//h1")), "Ожидалось, что заголовок будет видимым ");
+                        }
+                    }
                 }
             }
+
         }
 
-        [Test(Description = "3. Закрытие браузера"), Order(3)]
+        [Test(Description = "2. Закрытие браузера"), Order(2)]
         public void CloseBrowser()
         {
             driver.Quit();
---
+++ TheFirstAssignment/TheThirdClass/2.CheckStickers.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace TheFirstAssignment.TheThirdClass
+{
+    [TestFixture(Description = "2. Проверка наличия стикеров у товаров "), Order(2)]
+    class CheckStickers 
+    { 
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private By locator;
+
+        [Test(Description = "1. Открыть браузер"), Order(1)]
+        public void OpenBrowser()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("http://localhost/litecart/");
+            driver.Manage().Window.Maximize();
+
+            locator = By.XPath("(//li[contains(@class,'product')])");
+            IList<IWebElement> products = driver.FindElements(locator);
+            int count = driver.FindElements(locator).Count;
+            
+            for (int i=1;i<=count;i++)
+            {
+                locator = By.XPath(string.Format(("(//div[contains(@class,'sticker')])[{0}]"),i));
+                int countOfStickers = products[i-1].FindElements(locator).Count();
+                Assert.AreEqual(countOfStickers, 1, "Ожидалось, что на каждом товаре будет 1 стикер");
+            }
+
+        }
+        [Test(Description = "3. Закрытие браузера"), Order(3)]
+        public void CloseBrowser()
+        {
+            driver.Quit();
+            driver = null;
+
+        }
+    }
+}
